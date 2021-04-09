62b273c0adc6588a9c93bc7926eaea7f9aa3e2c0
ashulyak <alexey.shulyak@vrpconsulting.com>
2021-04-09 13:48:21 +0300

    refactor auth service

--- src/app/core/user/services/auth.service.ts
+++ src/app/core/user/services/auth.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {BehaviorSubject, Observable, of} from 'rxjs';
 import {first, map, switchMap, tap} from 'rxjs/operators';
-import { IUser } from '../../../shared/interfacces/user';
 import {CookieService} from "ngx-cookie-service";
 import {Router} from "@angular/router";
 import {AuthCtrlService} from "../../../backend-bridge/auth-ctrl/auth-ctrl.service";
+import {IUser} from "../../../shared/interfacces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  public user: Observable<any>;
-  private user$ = new BehaviorSubject<any>(undefined);
-  private TOKEN: string = null;
+  // public user: Observable<any>;
+  // private user$ = new BehaviorSubject<any>(undefined);
+
+  public token: Observable<any>;
+  private token$ = new BehaviorSubject<any>(undefined);
 
   constructor(private authCtrlService: AuthCtrlService, private cookieService: CookieService, private router: Router) {
     this.setUserMap();
@@ -21,8 +22,9 @@ export class AuthService {
 
   public login(user: IUser): Observable<{ token: string }> {
     return this.authCtrlService.login(user).pipe(
-      tap(({ token }) => {
-        this.setUser(token);
+      tap((user) => {
+        this.setToken(user.token);
+        // this.setUser(user.token);
       }),
     );
   }
@@ -31,63 +33,71 @@ export class AuthService {
     return this.authCtrlService.register(user);
   }
 
-  public isAuth(): boolean {
-    return !!this.TOKEN;
-  }
-
   public ifLoadUser() {
-    return this.user.pipe(
+    // return this.user.pipe(
+    //   first(),
+    //   map((user) => {
+    //     if (!user) {
+    //       this.router.navigateByUrl('/login');
+    //     }
+    //     return !!user;
+    //   }),
+    // );
+    return this.token.pipe(
       first(),
-      map((user) => {
-        if (!user) {
+      map((token) => {
+        if (!token) {
           this.router.navigateByUrl('/login');
         }
-        return !!user;
+        return !!token;
       }),
     );
   }
 
   private setUserMap() {
-    this.user = this.user$.asObservable().pipe(
-      switchMap((user) => {
-        const token = this.cookieService.get('token');
-        if (token && !user) {
-          return this.getUserByToken(token);
-        } else {
-          return of(user);
+    // this.user = this.user$.asObservable().pipe(
+    //   switchMap((user) => {
+    //     const token = this.cookieService.get('token');
+    //     if (token && !user) {
+    //       return this.getUserByToken(token);
+    //     } else {
+    //       return of(user);
+    //     }
+    //   }),
+    // );
+    this.token = this.token$.asObservable().pipe(
+      switchMap((token) => {
+        if (!token) {
+          return of(this.cookieService.get('token'));
         }
+        return of(token);
       }),
     );
   }
 
-  private getUserByToken(token: string) {
-    return of(null).pipe(
-      tap(() => {
-        this.setUser(true);
-      })
-    );
-  }
+  // private getUserByToken(token: string) {
+  //   return of(null).pipe(
+  //     tap(() => {
+  //       // this.setUser(true);
+  //     })
+  //   );
+  // }
 
-  private setUser(token: any) {
+  // private setUser(token: string) {
+  private setToken(token: string) {
     this.cookieService.delete('token', '/');
     if (token) {
       const dayLong = 1000 * 60 * 60 * 24;
       const tomorrow = new Date(Date.now() + dayLong);
-      this.cookieService.set('token', token.jwtToken, tomorrow, '/');
+      this.cookieService.set('token', token, tomorrow, '/');
     }
-    this.user$.next(true);
-  }
-
-  private setToken(token: string) {
-    this.TOKEN = token;
-  }
-
-  private getToken(): string {
-    return this.TOKEN;
+    this.token$.next(token);
+    // this.user$.next(token);
   }
 
   private logOut() {
-    this.setUser(null);
+    // this.setUser(null);
+    this.setToken(null);
     this.router.navigateByUrl('/login');
   }
 }
